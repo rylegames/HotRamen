40cafaa80eb699e4dca1ff067ea9d986a74bb84f
Ryan <ryanylee1996@gmail.com>
2016-08-03 23:37:50 -0400

    asdfasdf

--- app/bot/example.rb
+++ app/bot/example.rb
@@ -32,7 +32,7 @@ Bot.on :message do |message|
 
   when /add/i
     user = User.find_by(facebook_id: message.sender["id"])
-    # event_id = message.text.split(" ")[-1].to_i
+    event_id = message.text.split(" ")[-1].to_i
     # attendance = user.attend!(event_id)
     # attendance.save
 

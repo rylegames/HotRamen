593b1534d9e1d23d0fe43f88b89a624098e8c98c
Ryan <ryanylee1996@gmail.com>
2016-08-05 23:10:18 -0400

    asdf

--- app/bot/example.rb
+++ app/bot/example.rb
@@ -207,14 +207,16 @@ Bot.on :postback do |postback|
   when 'WELCOME_NEW_USER'
     text = "Welcome to upData, the bot with all the events for Harvard's Opening Days! Created by your classmate Ryan Lee '20. Text 'my events' to start building your schedule!"
   when /MORE_ALL_EVENTS/i
-    event_id = postback.payload.text.split("_")[-1].to_i
-    events = Event.all.limit(5).offset(event_id)
+    # event_id = postback.payload.text.split("_")[-1].to_i
+    # events = Event.all.limit(5).offset(event_id)
     Bot.deliver(
       recipient: message.sender,
       message: {
-        text: event_id.to_s
+        text: "event_id.to_s"
       }
     )
+
+    text = "asdf"
 
     # if events.length > 1
     #   events[0..-2].each do |event|

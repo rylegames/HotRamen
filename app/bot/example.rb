c685458971885de2ea51ebfa779c22b3854f8957
Ryan <ryanylee1996@gmail.com>
2016-08-07 22:18:24 -0400

    asdf

--- app/bot/example.rb
+++ app/bot/example.rb
@@ -29,191 +29,191 @@ Bot.on :message do |message|
       }
     )
 
-  when /help/i
-    text = ["Here's the list of commands",
-      "'all events' -> display all upcoming events",
-      "'my events' -> display my events in schedule",
-      "'show 1' -> show info of event with id, in this case 1",
-      "'add 1' -> add event to your schedule, in this case 1",
-      "'delete 1' -> delete event in your schedule, in this case 1",
-      "'help' -> Here's the list of commands ..."]
+  # when /help/i
+  #   text = ["Here's the list of commands",
+  #     "'all events' -> display all upcoming events",
+  #     "'my events' -> display my events in schedule",
+  #     "'show 1' -> show info of event with id, in this case 1",
+  #     "'add 1' -> add event to your schedule, in this case 1",
+  #     "'delete 1' -> delete event in your schedule, in this case 1",
+  #     "'help' -> Here's the list of commands ..."]
 
-    text.each do |piece|
-      Bot.deliver(
-        recipient: message.sender,
-        message: {
-          text: piece
-        }
-      )
-    end
+  #   text.each do |piece|
+  #     Bot.deliver(
+  #       recipient: message.sender,
+  #       message: {
+  #         text: piece
+  #       }
+  #     )
+  #   end
 
-  when /new account/i
-    begin
-      user = User.new(facebook_id: message.sender["id"])
-      user.save
+  # when /new account/i
+  #   begin
+  #     user = User.new(facebook_id: message.sender["id"])
+  #     user.save
 
-      Bot.deliver(
-        recipient: message.sender,
-        message: {
-          text: 'created!'
-        }
-      )
-    catch
-      Bot.deliver(
-        recipient: message.sender,
-        message: {
-          text: 'failed!'
-        }
-      )
-    end
+  #     Bot.deliver(
+  #       recipient: message.sender,
+  #       message: {
+  #         text: 'created!'
+  #       }
+  #     )
+  #   catch
+  #     Bot.deliver(
+  #       recipient: message.sender,
+  #       message: {
+  #         text: 'failed!'
+  #       }
+  #     )
+  #   end
 
-  when /add/i
-    user = User.find_by(facebook_id: message.sender["id"])
-    event_id = message.text.split(" ")[-1].to_i
-    event = Event.find(event_id)
+  # when /add/i
+  #   user = User.find_by(facebook_id: message.sender["id"])
+  #   event_id = message.text.split(" ")[-1].to_i
+  #   event = Event.find(event_id)
     
-    if event
-      attendance = user.attend!(event_id)
-      attendance.save
+  #   if event
+  #     attendance = user.attend!(event_id)
+  #     attendance.save
 
-      Bot.deliver(
-        recipient: message.sender,
-        message: {
-          text: 'Event has been added!'
-        }
-      )
-    else
-      Bot.deliver(
-        recipient: message.sender,
-        message: {
-          text: "Couldn't find that event. Double check the event number"
-        }
-      )
-    end
+  #     Bot.deliver(
+  #       recipient: message.sender,
+  #       message: {
+  #         text: 'Event has been added!'
+  #       }
+  #     )
+  #   else
+  #     Bot.deliver(
+  #       recipient: message.sender,
+  #       message: {
+  #         text: "Couldn't find that event. Double check the event number"
+  #       }
+  #     )
+  #   end
 
-  when /delete/i
-    user = User.find_by(facebook_id: message.sender["id"])
-    event_id = message.text.split(" ")[-1].to_i
-    user.unattend(event_id)
-    Bot.deliver(
-        recipient: message.sender,
-        message: {
-          text: 'Event has been deleted!'
-        }
-      )
+  # when /delete/i
+  #   user = User.find_by(facebook_id: message.sender["id"])
+  #   event_id = message.text.split(" ")[-1].to_i
+  #   user.unattend(event_id)
+  #   Bot.deliver(
+  #       recipient: message.sender,
+  #       message: {
+  #         text: 'Event has been deleted!'
+  #       }
+  #     )
 
-  when /show/i
-    event_id = message.text.split(" ")[-1].to_i
-    event = Event.find(event_id)
+  # when /show/i
+  #   event_id = message.text.split(" ")[-1].to_i
+  #   event = Event.find(event_id)
 
-    if event
-      event.full_display.each do |text|
-        Bot.deliver(
-          recipient: message.sender,
-          message: {
-            text: text
-          }
-        )
-      end
+  #   if event
+  #     event.full_display.each do |text|
+  #       Bot.deliver(
+  #         recipient: message.sender,
+  #         message: {
+  #           text: text
+  #         }
+  #       )
+  #     end
 
-      Bot.deliver(
-        recipient: message.sender,
-        message: {
-          "attachment": {
-            "type": "template",
-            "payload": {
-              "template_type": "generic",
-              "elements": {
-                "element": {
-                  "title": event.location,
-                  "image_url": "https://maps.googleapis.com/maps/api/staticmap?size=764x400&center="+event.latitude.to_s+","+event.longitude.to_s+"&zoom=25&markers="+event.latitude.to_s+","+event.longitude.to_s,
-                  "item_url": "http://maps.apple.com/maps?q="+event.latitude.to_s+","+event.longitude.to_s+"&z=16"
-                }
-              }
-            }
-          }
-        }
-      )
+  #     Bot.deliver(
+  #       recipient: message.sender,
+  #       message: {
+  #         "attachment": {
+  #           "type": "template",
+  #           "payload": {
+  #             "template_type": "generic",
+  #             "elements": {
+  #               "element": {
+  #                 "title": event.location,
+  #                 "image_url": "https://maps.googleapis.com/maps/api/staticmap?size=764x400&center="+event.latitude.to_s+","+event.longitude.to_s+"&zoom=25&markers="+event.latitude.to_s+","+event.longitude.to_s,
+  #                 "item_url": "http://maps.apple.com/maps?q="+event.latitude.to_s+","+event.longitude.to_s+"&z=16"
+  #               }
+  #             }
+  #           }
+  #         }
+  #       }
+  #     )
 
-    else
-      Bot.deliver(
-          recipient: message.sender,
-          message: {
-            text: "Couldn't find that event. Double check the event number"
-          }
-        )
-    end
+  #   else
+  #     Bot.deliver(
+  #         recipient: message.sender,
+  #         message: {
+  #           text: "Couldn't find that event. Double check the event number"
+  #         }
+  #       )
+  #   end
 
-  when /all events/i
-    #events = Event.all.where('begin_date > ?', DateTime.current - 30.minutes).order('id asc').take(5)
-    #events = Event.all.where('begin_date > ?', DateTime.current - 30.minutes).take(5)
-    events = Event.order(:id).where('begin_date > ?', DateTime.current - 30.minutes).limit(5).offset(0)
-    events[0..-2].each do |event|
-      Bot.deliver(
-        recipient: message.sender,
-        message: {
-          text: event.mini_display
-        }
-      )
-    end
+  # when /all events/i
+  #   #events = Event.all.where('begin_date > ?', DateTime.current - 30.minutes).order('id asc').take(5)
+  #   #events = Event.all.where('begin_date > ?', DateTime.current - 30.minutes).take(5)
+  #   events = Event.order(:id).where('begin_date > ?', DateTime.current - 30.minutes).limit(5).offset(0)
+  #   events[0..-2].each do |event|
+  #     Bot.deliver(
+  #       recipient: message.sender,
+  #       message: {
+  #         text: event.mini_display
+  #       }
+  #     )
+  #   end
 
-    Bot.deliver(
-      recipient: message.sender,
-      message:{
-        "attachment":{
-          "type":"template",
-          "payload":{
-            "template_type":"button",
-            "text": events[-1].mini_display,     
-            "buttons":[
-              {
-                "type":"postback",
-                "title":"More Events",
-                "payload":"MORE_ALL_EVENTS_" + 5.to_s
-              }              
-            ]
-          }
-        }
-      }
-    )
+  #   Bot.deliver(
+  #     recipient: message.sender,
+  #     message:{
+  #       "attachment":{
+  #         "type":"template",
+  #         "payload":{
+  #           "template_type":"button",
+  #           "text": events[-1].mini_display,     
+  #           "buttons":[
+  #             {
+  #               "type":"postback",
+  #               "title":"More Events",
+  #               "payload":"MORE_ALL_EVENTS_" + 5.to_s
+  #             }              
+  #           ]
+  #         }
+  #       }
+  #     }
+  #   )
 
-    if User.find_by(facebook_id: message.sender["id"]).events.size == 0
-      Bot.deliver(
-        recipient: message.sender,
-        message: {
-          text: "adsf"
-        }
-      )
-    end
+  #   if User.find_by(facebook_id: message.sender["id"]).events.size == 0
+  #     Bot.deliver(
+  #       recipient: message.sender,
+  #       message: {
+  #         text: "adsf"
+  #       }
+  #     )
+  #   end
 
-  when /my events/i
-    user = User.find_by(facebook_id: message.sender["id"])
-    if user and user.events.size > 0
-      user.events.each do |event|
-        Bot.deliver(
-          recipient: message.sender,
-          message: {
-            text: event.mini_display
-          }
-        )
-      end
-    else
-      Bot.deliver(
-          recipient: message.sender,
-          message: {
-            text: "Looks like you haven't added any events to your schedule.\nText 'all events' and see what's going on!"
-          }
-        )
-    end
+  # when /my events/i
+  #   user = User.find_by(facebook_id: message.sender["id"])
+  #   if user and user.events.size > 0
+  #     user.events.each do |event|
+  #       Bot.deliver(
+  #         recipient: message.sender,
+  #         message: {
+  #           text: event.mini_display
+  #         }
+  #       )
+  #     end
+  #   else
+  #     Bot.deliver(
+  #         recipient: message.sender,
+  #         message: {
+  #           text: "Looks like you haven't added any events to your schedule.\nText 'all events' and see what's going on!"
+  #         }
+  #       )
+  #   end
 
-  when /get started/i
-    User.create(facebook_id: message.sender["id"]) unless User.find_by(facebook_id: message.sender["id"])
-    Bot.deliver(
-        recipient: message.sender,
-        message: {
-          text: "New user."
-        }
-      )
+  # when /get started/i
+  #   User.create(facebook_id: message.sender["id"]) unless User.find_by(facebook_id: message.sender["id"])
+  #   Bot.deliver(
+  #       recipient: message.sender,
+  #       message: {
+  #         text: "New user."
+  #       }
+  #     )
 
   else
     Bot.deliver(

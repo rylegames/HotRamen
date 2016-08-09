77004d39fec465ab9df0cb2a1dfd2b55c30e7986
Ryan <ryanylee1996@gmail.com>
2016-08-09 00:31:03 -0400

    asdf

--- app/bot/example.rb
+++ app/bot/example.rb
@@ -9,11 +9,9 @@ require 'facebook/messenger'
 #     }
 #   ]
 # }' "https://graph.facebook.com/v2.6/me/thread_settings?access_token=EAASmNgjGL9sBAEjNsWIuB1SDX1VjExN9NgBWGNP4kl0ZC3wk3XdOuhQ4Ee2JH0KlzG2QKMrer7MbwEZCpryAUnWjzXTsFs4ZB8KG9NZA9EX772lIXUuHYEHHHN9l5PQ7awdqUttfkXMzJBzks8v17h1ZC5ZCmqyf7tco4RHODtzQZDZD"      
-Facebook::Messenger::Subscriptions.subscribe
 
 Facebook::Messenger.configure do |config|
   config.access_token = ENV['ACCESS_TOKEN']
-  config.app_secret =  ENV['FB_APP_SECRET_TOKEN']
   config.verify_token = ENV['VERIFY_TOKEN']
 end
 

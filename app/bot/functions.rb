a1b5b6687a175ba59b5207b03520c038ca8dd01d
Ryan <ryanylee1996@gmail.com>
2016-08-14 12:56:53 -0400

    v1

--- app/bot/functions.rb
+++ app/bot/functions.rb
@@ -241,6 +241,7 @@ end
 def my_events(sender, event_id)
   user = User.find_by(facebook_id: sender["id"])
   events = user.events.where('begin_date > ?', DateTime.current - 30.minutes).order('id asc').limit(5).offset(event_id)
+  quick_replies = Array.new
   quick_replies = [{ "content_type":"text", "title":"More Events", "payload":"MY_#{event_id + 5}"}]
   if user and events.size > 0
     events[0..-2].each do |event|
@@ -253,6 +254,8 @@ def my_events(sender, event_id)
 
       quick_replies.push({ "content_type":"text", "title":"#{event.id}","payload":"SHOW_#{event.id}"})
     end
+    quick_replies.push({ "content_type":"text", "title":"#{events[-1].id}","payload":"SHOW_#{events[-1].id}"})
+
     Bot.deliver(
       recipient: sender,
       message:{
